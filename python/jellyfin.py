e0d3b38366939de1c43516a0e1f61b7124a4a2bb
iwantmyhatback <iwantmyhatbackk@gmail.com>
2026-02-07 18:37:55 -0500

    changes for jellyfin API update

--- python/jellyfin.py
+++ python/jellyfin.py
@@ -1,11 +1,14 @@
 from operator import itemgetter
-from utilities import isAquiredWithinThreshold, stringToDate, replaceEveryNth, getLastUrlSegment
+from utilities import isAquiredWithinThreshold, replaceEveryNth, getLastUrlSegment
 from json import load as loadJson, dumps as dumpsJson, dump as dumpJson
-from requests import get as httpGET
 from htmlProcessing import main as generateHtml
-from jellyfin_apiclient_python import JellyfinClient
 from os import environ as osEnviron, path as osPath
 import logging as log
+
+import jellyfin_api
+from jellyfin_api.api.user_api import UserApi
+from jellyfin_api.api.items_api import ItemsApi
+from jellyfin_api.api.remote_image_api import RemoteImageApi
 
 
 #################
@@ -14,6 +17,8 @@ import logging as log
 
 # Check whether we want to use secondary JSON file (mostly used for testing on a secondary machine)
 REMOTE_CONNECTION = str(osEnviron.get("REMOTE_CONNECTION")).upper()
+DEVICE = osEnviron.get("DEVICE") or "defaultDevice"
+DEVICE_ID = osEnviron.get("DEVICE_ID") or "defaultDeviceId"
 
 # Load the appropriate JSON file of configurations
 if REMOTE_CONNECTION in ["YES", "TRUE"]:
@@ -43,49 +48,65 @@ def jellyfinConnection():
     CLIENT_SECRET_FILE = JELLYFIN.get("CLIENT_SECRET_FILE")
     CREDENTIAL_FILE = f"{CREDENTIAL_LOCATION}/{CLIENT_SECRET_FILE}"
 
-    client = JellyfinClient()
-    client.config.app('jellyfin.supplemental',
-                      '0.0.1',
-                      'administratorsMachine',
-                      'adminMach'
-                      )
-    client.config.data["auth.ssl"] = True
+    configuration = jellyfin_api.Configuration(host=f"{SERVER_URL}:{PORT}")
 
-    if not osPath.isfile(CREDENTIAL_FILE):
-        client.auth.connect_to_address(f"{SERVER_URL}:{PORT}")
-        client.auth.login(f"{SERVER_URL}:{PORT}", USERNAME, PASSWORD)
-        credentials = client.auth.credentials.get_credentials()
-        server = credentials["Servers"][0]
-        server["username"] = USERNAME
-        with open(CREDENTIAL_FILE, 'w', encoding='utf-8') as file:
-            dumpJson(server, file, ensure_ascii=False, indent=4)
+    if osPath.isfile(CREDENTIAL_FILE):
+        credentials = loadJson(open(CREDENTIAL_FILE))
+        token = credentials["AccessToken"]
+        user_id = credentials["UserId"]
+    else:
+        # Initial auth — the authenticate endpoint doesn't include auth settings
+        # in the generated client, so we pass the header manually
+        auth_header = (
+            'MediaBrowser Client="jellyfin.supplemental", '
+            'Device="administratorsMachine", '
+            'DeviceId="adminMach", '
+            'Version="0.0.1"'
+        )
+        api_client = jellyfin_api.ApiClient(configuration)
+        auth_api = UserApi(api_client)
+        result = auth_api.authenticate_user_by_name(
+            jellyfin_api.AuthenticateUserByName(username=USERNAME, pw=PASSWORD),
+            _headers={'Authorization': auth_header}
+        )
+        token = result.access_token
+        user_id = str(result.user.id)
+        with open(CREDENTIAL_FILE, 'w', encoding='utf-8') as f:
+            dumpJson({"AccessToken": token, "UserId": user_id, "username": USERNAME}, f, indent=4)
 
-    credentialFile = open(CREDENTIAL_FILE)
-    credentials = loadJson(credentialFile)
-    client.authenticate({"Servers": [credentials]}, discover=False)
-    log.debug(f'[RETURN] connection/jellyfinConnection : {client}')
-    return client
+    # Set auth header with token for all subsequent calls
+    configuration.api_key['CustomAuthentication'] = (
+        f'MediaBrowser Token="{token}", '
+        f'Client="jellyfin.supplemental", '
+        f'Device="{DEVICE}", '
+        f'DeviceId="{DEVICE_ID}", '
+        f'Version="1.0.0"'
+    )
+    api_client = jellyfin_api.ApiClient(configuration)
+    log.debug(f'[RETURN] connection/jellyfinConnection : {api_client}')
+    return api_client, user_id
 
 
 # parseMediaType() Returns the specifics for query depending on the type of media (TV and Movies are determined to be new differently)
 def parseMediaType(queryType):
     log.debug(f'[FUNCTION] jellyfin/parseMediaType({queryType})')
-    defaultFields = ['Tags',
-                     'Genres',
-                     'ProviderIds',
-                     'RemoteTrailers',
-                     'Overview'
-                     ]
+    defaultFields = [jellyfin_api.ItemFields.TAGS,
+                     jellyfin_api.ItemFields.GENRES,
+                     jellyfin_api.ItemFields.PROVIDERIDS,
+                     jellyfin_api.ItemFields.REMOTETRAILERS,
+                     jellyfin_api.ItemFields.OVERVIEW]
     mediaSpecifics = {
         'Movie': {
             'name': 'Movie',
-            'mediaTypesList': ['Movie'],
-            'fields': ['DateCreated', *defaultFields]
+            'mediaTypesList': [jellyfin_api.BaseItemKind.MOVIE],
+            'fields': [jellyfin_api.ItemFields.DATECREATED, *defaultFields],
+            'sortBy': [jellyfin_api.ItemSortBy.DATECREATED]
         },
         'Series': {
             'name': 'Series',
-            'mediaTypesList': ['Series'],
-            'fields': ['DateLastMediaAdded', *defaultFields]
+            'mediaTypesList': [jellyfin_api.BaseItemKind.SERIES],
+            'fields': [jellyfin_api.ItemFields.DATELASTMEDIAADDED, *defaultFields],
+            'sortBy': [jellyfin_api.ItemSortBy.DATELASTCONTENTADDED]
         }
     }
 
@@ -96,7 +117,7 @@ def parseMediaType(queryType):
         raise ValueError()
 
     log.debug(
-        f'[RETURN] jellyfin/parseMediaType : \n{dumpsJson(mediaSpecifics.get(queryType), indent=2)}')
+        f'[RETURN] jellyfin/parseMediaType : {mediaSpecifics.get(queryType)}')
     return mediaSpecifics.get(queryType)
 
 
@@ -105,62 +126,67 @@ def queryJellyfin(queryTypeSpecifics):
     log.debug(f'[FUNCTION] jellyfin/queryJellyfin({queryTypeSpecifics})')
 
     try:
-        recentlyAddedItems = client.jellyfin.user_items(
-            handler="/Latest",
-            params={
-                'fields': queryTypeSpecifics.get('fields'),
-                'includeItemTypes': queryTypeSpecifics.get('mediaTypesList'),
-                'limit': 1000
-            }
+        items_api = ItemsApi(api_client)
+        result = items_api.get_items(
+            user_id=user_id,
+            fields=queryTypeSpecifics.get('fields'),
+            include_item_types=queryTypeSpecifics.get('mediaTypesList'),
+            sort_by=queryTypeSpecifics.get('sortBy'),
+            sort_order=[jellyfin_api.SortOrder.DESCENDING],
+            recursive=True,
+            limit=50,
+            _request_timeout=30
         )
-    except:
+        recentlyAddedItems = result.items or []
+    except Exception:
         log.error(
             f'[ERROR] There was an error querying latest {queryTypeSpecifics.get("name")}')
         raise RuntimeError()
     log.debug(
-        f'[RETURN] jellyfin/queryJellyfin :\n{dumpsJson(recentlyAddedItems, indent=2)}')
+        f'[RETURN] jellyfin/queryJellyfin : {len(recentlyAddedItems)} items')
     return recentlyAddedItems
 
 
 # extractItemData() extracts the data for the email from the Jellyfin response data and formats it into a list of uniform objects
 def extractItemData(recentlyAddedItems, queryTypeSpecifics):
     log.debug(
-        f'[FUNCTION] jellyfin/extractItemData(\n{dumpsJson(recentlyAddedItems, indent=2)}, \n{dumpsJson(queryTypeSpecifics, indent=2)})')
+        f'[FUNCTION] jellyfin/extractItemData({len(recentlyAddedItems)} items)')
     newItemList = []
     baseYoutube = "https://www.youtube.com/watch?v={trailerId}"
 
     for item in recentlyAddedItems:
-        log.debug(f'[VALUE] item : \n{dumpsJson(item, indent=2)}')
+        log.debug(f'[VALUE] item : \n{item.model_dump_json(indent=2)}')
 
         # Timeframe relevance check
-        if item.get("DateCreated"):
-            relevanceDate = item.get("DateCreated")
-            log.debug(f'[VALUE] item.DateCreated : {relevanceDate}')
+        if item.date_created:
+            relevanceDate = item.date_created
+            log.debug(f'[VALUE] item.date_created : {relevanceDate}')
 
-        elif item.get("DateLastMediaAdded"):
-            relevanceDate = item.get("DateLastMediaAdded")
-            log.debug(f'[VALUE] item.DateLastMediaAdded : {relevanceDate}')
+        elif item.date_last_media_added:
+            relevanceDate = item.date_last_media_added
+            log.debug(f'[VALUE] item.date_last_media_added : {relevanceDate}')
 
         else:
             log.warning(
-                f'[VALUE] item.name: {item.get("Name")} has no item.DateCreated OR item.DateLastMediaAdded')
+                f'[VALUE] item.name: {item.name} has no date_created OR date_last_media_added')
             continue
 
-        addedDate = stringToDate(relevanceDate, "%Y-%m-%dT%H:%M:%S.%f")
+        # date_created and date_last_media_added are already datetime objects
+        addedDate = relevanceDate.replace(tzinfo=None)
 
         if isAquiredWithinThreshold(addedDate):
 
             # Title, Overview, Tag List, Genre List
-            itemTitle = item.get("Name")
+            itemTitle = item.name
             log.debug(f'[VALUE] itemTitle : {itemTitle}')
 
-            itemOverview = item.get("Overview")
+            itemOverview = item.overview
             log.debug(f'[VALUE] itemOverview : {itemOverview}')
 
-            itemTagList = ', '.join(item.get("Tags"))
+            itemTagList = ', '.join(item.tags) if item.tags else ''
             log.debug(f'[VALUE] itemTagList : {itemTagList}')
 
-            itemGenreList = ', '.join(item.get("Genres"))
+            itemGenreList = ', '.join(item.genres) if item.genres else ''
             log.debug(f'[VALUE] itemGenreList : {itemGenreList}')
 
             # Movie Poster URL
@@ -168,72 +194,70 @@ def extractItemData(recentlyAddedItems, queryTypeSpecifics):
             log.debug(f'[VALUE] itemPosterUrl : {itemPosterUrl}')
 
             # Movie Release Year
-            if item.get("PremiereDate"):
-                itemPremierDate = stringToDate(
-                    item.get("PremiereDate"), "%Y-%m-%dT%H:%M:%S.%f")
-                itemYear = itemPremierDate.year
+            if item.premiere_date:
+                itemYear = item.premiere_date.year
 
             else:
                 log.debug(
-                    f'[VALUE] item.name: {item.get("Name")} has no item.PremiereDate')
+                    f'[VALUE] item.name: {item.name} has no premiere_date')
                 itemYear = 0000
 
             log.debug(f'[VALUE] itemYear : {itemYear}')
 
             # Content Rating
-            if item.get("OfficialRating"):
-                itemContentRating = item.get("OfficialRating")
+            if item.official_rating:
+                itemContentRating = item.official_rating
 
             else:
                 itemContentRating = 'Unknown'
                 log.debug(
-                    f'[VALUE] item.name: {item.get("Name")} has no item.OfficialRating')
+                    f'[VALUE] item.name: {item.name} has no official_rating')
 
             log.debug(f'[VALUE] itemContentRating : {itemContentRating}')
 
             # Critic Rating
-            if item.get("CriticRating"):
+            if item.critic_rating:
                 itemCriticRating = "Critics: {}% ".format(
-                    item.get("CriticRating"))
+                    item.critic_rating)
 
             else:
                 itemCriticRating = ''
                 log.debug(
-                    f'[VALUE] item.name: {item.get("Name")} has no item.CriticRating')
+                    f'[VALUE] item.name: {item.name} has no critic_rating')
 
             log.debug(f'[VALUE] itemCriticRating : {itemCriticRating}')
 
             # Community Rating
-            if item.get("CommunityRating"):
+            if item.community_rating:
                 withCritic = '& ' if itemCriticRating != '' else ''
-                roundedRating = round(float(item.get("CommunityRating")), 1)
+                roundedRating = round(float(item.community_rating), 1)
                 itemCommunityRating = "Community: {}/10 {}".format(
                     roundedRating, withCritic)
 
             else:
                 itemCommunityRating = ''
                 log.debug(
-                    f'[VALUE] item.name: {item.get("Name")} has no item.CommunityRating')
+                    f'[VALUE] item.name: {item.name} has no community_rating')
 
             log.debug(f'[VALUE] itemCommunityRating : {itemCommunityRating}')
 
             # Trailer URL
             itemTrailerUrl = baseYoutube.format(trailerId='dQw4w9WgXcQ')
-            if len(item.get("RemoteTrailers")) > 0:
-                trailerElement = item.get("RemoteTrailers")[0]
-                trailerUrl = trailerElement.get('Url')
+            if item.remote_trailers and len(item.remote_trailers) > 0:
+                trailerElement = item.remote_trailers[0]
+                trailerUrl = trailerElement.url
 
-                if 'watch?v=' in trailerUrl:
+                if trailerUrl and 'watch?v=' in trailerUrl:
                     trailerId = trailerUrl.rsplit('watch?v=', 1)[-1]
                     itemTrailerUrl = baseYoutube.format(trailerId=trailerId)
 
-                elif 'video_id=' in trailerUrl:
+                elif trailerUrl and 'video_id=' in trailerUrl:
                     trailerId = trailerUrl.rsplit('video_id=', 1)[-1]
                     itemTrailerUrl = baseYoutube.format(trailerId=trailerId)
 
             else:
                 log.debug(
-                    f'[VALUE] item.name: {item.get("Name")} has no item.RemoteTrailers')
+                    f'[VALUE] item.name: {item.name} has no remote_trailers')
                 log.debug(
                     f'[VALUE] using default itemTrailerUrl (rickroll) : {itemTrailerUrl}')
 
@@ -265,17 +289,15 @@ def extractItemData(recentlyAddedItems, queryTypeSpecifics):
 # getRemoteImage() gets a poster image from TMDB
 def getRemoteImage(item):
     log.debug(
-        f'[FUNCTION] jellyfin/getRemoteImage(\n{dumpsJson(item, indent=2)})')
+        f'[FUNCTION] jellyfin/getRemoteImage({item.name})')
     baseImageUrl = "http://image.tmdb.org/t/p/w185/{imageId}"
-    itemId = item.get('Id')
 
-    response = client.jellyfin.items(
-        handler=f"/{itemId}/RemoteImages/",
-        params={
-            'providerName': 'TheMovieDb',
-            'type': 'Primary',
-            'includeAllLanguages': False
-        }
+    img_api = RemoteImageApi(api_client)
+    response = img_api.get_remote_images(
+        item_id=item.id,
+        type=jellyfin_api.ImageType.PRIMARY,
+        provider_name='TheMovieDb',
+        include_all_languages=False
     )
 
     finalImage = {
@@ -284,12 +306,12 @@ def getRemoteImage(item):
         'votes': -1,
     }
 
-    for image in response.get('Images'):
+    for image in response.images:
 
-        if image.get('CommunityRating') > finalImage.get('rating') and image.get('VoteCount') >= finalImage.get('votes'):
-            imageFile = getLastUrlSegment(image.get('Url'))
-            imageRating = image.get('CommunityRating')
-            imageVotes = image.get('VoteCount')
+        if image.community_rating > finalImage.get('rating') and image.vote_count >= finalImage.get('votes'):
+            imageFile = getLastUrlSegment(image.url)
+            imageRating = image.community_rating
+            imageVotes = image.vote_count
 
             finalImage.update(imageFile=imageFile)
             finalImage.update(rating=imageRating)
@@ -301,7 +323,7 @@ def getRemoteImage(item):
         return notFoundUrl
 
     else:
-        formattedUrl = baseImageUrl.format(imageId=imageFile)
+        formattedUrl = baseImageUrl.format(imageId=finalImage.get('imageFile'))
         log.debug(f'[RETURN] jellyfin/getRemoteImage : {formattedUrl}')
         return formattedUrl
 
@@ -310,7 +332,7 @@ def getRemoteImage(item):
 ## Executions ##
 ################
 
-client = jellyfinConnection()
+api_client, user_id = jellyfinConnection()
 
 
 # main() Performs the functionality of this file. Query Jellyfin for infomation, extract the data, and trandform it into emailable format
--- shell/main.sh
+++ shell/main.sh
@@ -26,7 +26,44 @@ fi
 
 . "${PYENV_LOCATION}/bin/activate"
 
-pip install --quiet --requirement requirements.txt
+# Determine which info file to use (mirrors logic in python/jellyfin.py)
+if [ "${REMOTE_CONNECTION:-}" = "TRUE" ] || [ "${REMOTE_CONNECTION:-}" = "YES" ]; then
+    INFO_FILE="${REPO_ROOT_DIR}/configuration/info.remote.json"
+else
+    INFO_FILE="${REPO_ROOT_DIR}/configuration/info.json"
+fi
+
+JELLYFIN_URL=$(python3 -c "
+import json
+info = json.load(open('${INFO_FILE}'))
+j = info['JELLYFIN']
+print(f\"{j['SERVER_URL']}:{j['PORT']}\")
+")
+
+JELLYFIN_CLIENT_DIR="${PYENV_LOCATION}/jellyfin-client"
+
+if [ ! -d "${JELLYFIN_CLIENT_DIR}" ] || [ "${FORCE_CLIENT_REGEN:-}" = 'TRUE' ]; then
+    echo "[INFO] [OPENAPI] Generating Jellyfin API client from ${JELLYFIN_URL}/api-docs/openapi.json"
+    openapi-generator generate \
+        -i "${JELLYFIN_URL}/api-docs/openapi.json" \
+        -g python \
+        -o "${JELLYFIN_CLIENT_DIR}" \
+        --package-name jellyfin_api \
+        --skip-validate-spec
+
+    # Patch: pydantic validate_call strips enums to str, but serializer calls .value
+    sed -i.bak 's/type\.value/type.value if hasattr(type, '\''value'\'') else type/g' \
+        "${JELLYFIN_CLIENT_DIR}/jellyfin_api/api/remote_image_api.py"
+    rm -f "${JELLYFIN_CLIENT_DIR}/jellyfin_api/api/remote_image_api.py.bak"
+
+    pip install "${JELLYFIN_CLIENT_DIR}"
+else
+    echo "[INFO] [OPENAPI] Jellyfin API client already exists at ${JELLYFIN_CLIENT_DIR}"
+fi
+
+export HOSTNAME="$(hostname)"
+
+pip install --requirement requirements.txt
 # pip freeze > requirements.txt
 
 python3 -Bu python/main.py
